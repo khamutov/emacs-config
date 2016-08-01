157fd11a64e9944913c21b38b5e0589be0695933
Aleksandr Khamutov <khamutov@ailabs.ru>
2016-08-01 20:33:21 +0300

    add cua mode (copy-paste-cut)

--- customizations/editing.el
+++ customizations/editing.el
@@ -118,3 +118,8 @@
 ;; If some other layout is used replace russian-computer with
 ;; preferable input method
 (reverse-input-method 'russian-computer)
+
+(cua-mode t)
+(setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
+(transient-mark-mode 1) ;; No region when it is not highlighted
+(setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
